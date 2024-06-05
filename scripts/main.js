360fae9c849499aaedb9335c9cb66f2325445642
danil.l <danil.l@zimalab.com>
2024-06-05 12:19:29 +0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function online() {
     const online = document.querySelector('#online')
     const options = { hour: '2-digit', timeZone: 'Europe/Moscow' }
@@ -5,4 +7,47 @@ function online() {
     if (hours >= 9 && hours <= 20) online.dataset.active = ''
 }
 
+function form() {
+    const form = document.querySelector('#form')
+    const pages = [...form.children]
+
+    pages.forEach((page, index) => {
+        page.addEventListener('click', event => {
+            const action = event.target.dataset.action
+            if (action === 'next') {
+                if (page.dataset.valid !== '') return
+                if (index === pages.length - 1) return
+                delete pages[index].dataset.active
+                pages[index + 1].dataset.active = ''
+            }
+        })
+
+        page.addEventListener('input', event => {
+            if (event.target.type === 'text') {
+                if (event.target.value.length) {
+                    page.dataset.valid = ''
+                } else {
+                    delete page.dataset.valid
+                }
+            }
+            if (event.target.type === 'radio') {
+                page.dataset.valid = ''
+            }
+        })
+    })
+
+    form.querySelector('#amount')
+        .addEventListener('keypress', event => {
+            if (!/[0-9]/.test(event.key)) {
+                event.preventDefault()
+            }
+        })
+    
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+        // handle form submit
+    })
+}
+
 online()
+form()
